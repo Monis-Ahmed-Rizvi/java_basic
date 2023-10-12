b8dcd9f143430d9c84ba04f36f35972450656f36
Monis-Ahmed-Rizvi <monisahmedrizvi@gmail.com>
2023-10-12 23:55:06 +0530

    think java

--- t_java/abs_val.java
+++ t_java/abs_val.java
@@ -1,5 +1,36 @@
+// absolute value
+
 package t_jav;
+
+import java.util.*;
 
 public class abs_val {
 
+	public static double abs(double num)
+	{
+		
+		if(num<0)
+		{
+			
+			return -num;
+		}
+		else
+		{
+			return num;
+		}
+		
+		// dead code 
+	}
+	
+	
+	public static void main(String[] args)
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		double num = sc.nextInt();
+		System.out.println(abs(num));
+		
+		
+	}
+	
 }
